3b09f932be695f5d7f4e48225a49e73345a9ea70
Daksh987 <duckcheese23@gmail.com>
2025-02-26 15:08:49 -0500

    Update input fields

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -6,7 +6,7 @@ function Login(){
   return(
     <div id="loginDiv">
       <span id="inner-title">PLEASE LOG IN</span><br />
-      <input type="text" id="loginName" placeholder="Username" /><br />
+      <input type="text" id="loginName" placeholder="Email" /><br />
       <input type="password" id="loginPassword" placeholder="Password" /><br />
       <input type="submit" id="loginButton" className="buttons" value ="Do It" onClick={doLogin} />
       <span id="loginResult"></span>
