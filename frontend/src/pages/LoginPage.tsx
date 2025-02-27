28e5632895322232b434adf460e9deb2281954d4
Daksh987 <duckcheese23@gmail.com>
2025-02-27 09:47:07 -0500

    Change Background

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,10 +1,11 @@
 import PageTitle from '../components/PageTitle.tsx';
 import Login from '../components/Login.tsx';
+import app from './App.module.css';
 
 
 const LoginPage = () => {
   return (
-    <div id = "LPage">
+    <div className={app.LDiv}>
       <PageTitle />
       <Login />
     </div>
