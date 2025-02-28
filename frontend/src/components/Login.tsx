0d9151e6b40fe19ad53f98329324ad00e8e56b1c
luke-ded <luke@dederich.org>
2025-02-28 17:12:53 -0500

    Rough login page ui

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -1,3 +1,5 @@
+import app from "../pages/App.module.css";
+
 function Login(){
   function doLogin(event:any) : void {
     event.preventDefault();
@@ -5,10 +7,12 @@ function Login(){
   }
   return(
     <div id="loginDiv">
-      <span id="inner-title">PLEASE LOG IN</span><br />
-      <input type="text" id="loginName" placeholder="Email" /><br />
-      <input type="password" id="loginPassword" placeholder="Password" /><br />
-      <input type="submit" id="loginButton" className="buttons" value ="Login" onClick={doLogin} />
+      <span id="inner-title">LOG IN</span><br />
+      <h5 className={app.loginlabel}>Email</h5>
+      <input type="text" id="loginName" className = {app.logininputs} placeholder="Email" /><br />
+      <h5 className={app.loginlabel}>Password</h5>
+      <input type="password" id="loginPassword" className = {app.logininputs} placeholder="Password" /><br />
+      <button className={app.loginbuttons} id={app.dologinbutton} onClick={doLogin}>Login</button>
       <span id="loginResult"></span>
     </div>
   );
--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -2,13 +2,14 @@ import PageTitle from '../components/PageTitle.tsx';
 import Login from '../components/Login.tsx';
 import app from './App.module.css';
 import NavBar from '../components/NavBar.tsx';
+import logo from '../assets/testlogo.png'
 
 const LoginPage = () => {
   return (
     <div>
       <NavBar />
         <div className= {app.LDiv}>
-          <PageTitle />
+        <img src = {logo} id = {app.loginlogoimg}></img>
           <Login />
         </div>
     </div>
