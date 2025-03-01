78af21cad9534d69de373028f004e7b79c3206bd
Daksh987 <duckcheese23@gmail.com>
2025-03-01 13:02:05 -0500

    Allowing error handling for forgot password

--- frontend/src/components/ForgotPass.tsx
+++ frontend/src/components/ForgotPass.tsx
@@ -6,6 +6,25 @@ function ForgotPass(){
 
 
     function reclaimPass(event:any) : void{
+
+        const Email = (document.getElementById("loginName") as HTMLInputElement).value;
+        const alertMessage = document.getElementById("alertmessage");
+
+
+        if (alertMessage) {
+
+            if (Email.length == 0){
+                
+                alertMessage.innerText = "No Email is entered";
+                alertMessage.style.visibility = "visible";
+
+            } else {
+    
+                alertMessage.style.visibility = "hidden";
+            }
+
+        }
+
         event.preventDefault();
     }
 
@@ -15,7 +34,8 @@ function ForgotPass(){
         <div id = "loginDiv">
             <span id="inner-title">Forgot Password</span><br />
             <h5 className={app.loginlabel}>Enter your Email</h5>
-            <input type="text" id="loginName" className = {app.logininputs} placeholder="" /><br/>
+            <input type="text" id="loginName" className = {app.logininputs} placeholder="Email" /><br/>
+            <h5 id = "alertmessage"></h5>
             <button className={app.loginbuttons} id={app.dologinbutton} onClick={reclaimPass}>Reset Password</button>
         </div>
     );
