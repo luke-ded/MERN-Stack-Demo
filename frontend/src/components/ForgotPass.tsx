88a83fbb6bbae457fcae6e64f54d97521a47747b
Daksh987 <duckcheese23@gmail.com>
2025-03-01 12:45:37 -0500

    Create forgot password page

--- frontend/src/components/ForgotPass.tsx
+++ frontend/src/components/ForgotPass.tsx
@@ -1,11 +1,21 @@
 import app from "../pages/App.module.css";
-//import { useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 
 function ForgotPass(){
 
+    const navigate = useNavigate();
+
     function reclaimPass(event:any) : void{
         event.preventDefault();
+    }
+
+    function navLogin() {
+
+        if (location.pathname != '/login')
+            navigate('/login');// Change to forgot password page once set up
+        else 
+            navigate('/');
     }
 
     return(
@@ -14,6 +24,7 @@ function ForgotPass(){
             <h5 className={app.loginlabel}>Enter your Email</h5>
             <input type="text" id="loginName" className = {app.logininputs} placeholder="" /><br/>
             <button className={app.loginbuttons} id={app.dologinbutton} onClick={reclaimPass}>Reset Password</button>
+            <button className={app.loginbuttons} id={app.dologinbutton} onClick={navLogin}>Back to Login</button>
         </div>
     );
 }
