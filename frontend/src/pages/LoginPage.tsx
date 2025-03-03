2ad959e2dabad7a0d8008bdac96570128ffdd703
luke-ded <luke@dederich.org>
2025-03-03 09:23:39 -0500

    Signup redirect

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -2,14 +2,26 @@ import Login from '../components/Login.tsx';
 import app from './App.module.css';
 import NavBar from '../components/NavBar.tsx';
 import logo from '../assets/testlogo.png'
+import { useNavigate } from 'react-router-dom';
 
 const LoginPage = () => {
+  const navigate = useNavigate();
+
+  function navSignup()
+  {
+    navigate('/signup');
+  }
+
   return (
     <div>
       <NavBar />
         <div className= {app.LDiv}>
         <img src = {logo} id = {app.loginlogoimg}></img>
           <Login />
+        </div>
+        <div id={app.signinredirect}>
+          <p>Don't have an account?</p>
+          <button id={app.signinredirectbutton} onClick={navSignup}> Sign up.</button>
         </div>
     </div>
   );
