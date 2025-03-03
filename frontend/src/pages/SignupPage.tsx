e71ca9d342b03363bf39f593060a79fc81306058
luke-ded <luke@dederich.org>
2025-03-03 14:59:30 -0500

    Sign up redirect

--- frontend/src/pages/SignupPage.tsx
+++ frontend/src/pages/SignupPage.tsx
@@ -2,8 +2,16 @@ import app from './App.module.css';
 import NavBar from '../components/NavBar.tsx';
 import logo from '../assets/testlogo.png'
 import Signup from '../components/Signup.tsx';
+import { useNavigate } from 'react-router-dom';
 
 const SignupPage = () => {
+  const navigate = useNavigate();
+
+  function navLogin()
+  {
+    navigate('/login');
+  }
+
   return (
     <div>
       <NavBar />
@@ -11,6 +19,10 @@ const SignupPage = () => {
         <img src = {logo} id = {app.loginlogoimg}></img>
             <Signup />
         </div>
+        <div id={app.signupredirect}>
+        <p id={app.signinredirectmsg}>Already have an account?</p>
+        <button id={app.signinredirectbutton} onClick={navLogin}> Log in.</button>
+      </div>
     </div>
   );
 };
