318b09b03f6cc1b0f9a044ca9ab69619f27e80b1
Daksh987 <duckcheese23@gmail.com>
2025-03-03 21:42:32 -0500

    Fix 'Enter' button bug and modify validating email

--- frontend/src/components/ForgotPass.tsx
+++ frontend/src/components/ForgotPass.tsx
@@ -1,12 +1,11 @@
 import app from "../pages/App.module.css";
 //import { useNavigate } from 'react-router-dom';
-import {useState} from 'react'
+
 
 
 function ForgotPass(){
 
-    const [isValidEmail, setIsValidEmail] = useState(false);
-
+    
     function reclaimPass(){
 
         const Email = (document.getElementById("loginName") as HTMLInputElement).value;
@@ -23,9 +22,8 @@ function ForgotPass(){
             } else {
     
                 alertMessage.style.visibility = "hidden";
-                validateEmail();
-
-                if (isValidEmail){
+        
+                if (validateEmail()){
 
                     alertMessage.innerText = "Sent to " + Email;
                     alertMessage.style.visibility = "visible";
@@ -41,27 +39,13 @@ function ForgotPass(){
         
     }
 
-    function validateEnter(){
-    
-        const Input = document.getElementById("loginName");
-        Input?.addEventListener("keyup", function(event){
-
-            if (event.key === "Enter") {
-                event.preventDefault();
-                reclaimPass();
-            }
-        });
-    
-   }
-
+   
    function validateEmail(){
-    const ret = String((document.getElementById("loginName") as HTMLInputElement).value)
-    .toLowerCase()
-    .match(
-      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-    );
-
-    setIsValidEmail(Boolean(ret));
+        return String((document.getElementById("loginName") as HTMLInputElement).value)
+        .toLowerCase()
+        .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        );
    }
 
    
@@ -70,7 +54,7 @@ function ForgotPass(){
         <div id = "loginDiv">
             <span id="inner-title">Forgot Password</span><br />
             <h5 className={app.loginlabel}>Enter your Email</h5>
-            <input type="text" id="loginName" className = {app.logininputs} placeholder="Email" onKeyUp={validateEnter}/><br/>
+            <input type="text" id="loginName" className = {app.logininputs} placeholder="Email" onKeyUp={(e) => e.key === "Enter" && reclaimPass()}/><br/>
             <h5 id = "alertmessage"></h5>
             <button className={app.loginbuttons} id={app.dologinbutton} onClick={reclaimPass}>Confirm</button>
         </div>
