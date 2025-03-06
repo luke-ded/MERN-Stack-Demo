9baf09dcfb78913daa70e7083a06e34ac34cb84b
KentaF1202 <kentaf1202@gmail.com>
2025-03-06 17:17:09 -0500

    API part 2

--- api.js
+++ api.js
@@ -1,4 +1,6 @@
-const express = require('express');
+require('express');
+require('mongodb');
+/*const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
@@ -11,71 +13,74 @@ const MongoClient = require('mongodb').MongoClient;
 const url = 'mongodb+srv://kentaf1202:Lihys2G76A1hS6Ld@salvagefinancialdb.pvcx6.mongodb.net/SalvageFinancialDB?retryWrites=true&w=majority';
 
 const client = new MongoClient(url);
-client.connect();
+client.connect();*/
 
-app.use((req, res, next) =>
+exports.setApp = function ( app, client )
 {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-    );
-    res.setHeader(
-    'Access-Control-Allow-Methods',
-    'GET, POST, PATCH, DELETE, OPTIONS'
-    );
+    app.use((req, res, next) =>
+    {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+        );
+        res.setHeader(
+        'Access-Control-Allow-Methods',
+        'GET, POST, PATCH, DELETE, OPTIONS'
+        );
 
-    next();
-});
+        next();
+    });
 
-app.get('/api/login', async (req, res) => {
-    try {
-		const {email, password } = req.body;
+    app.get('/api/login', async (req, res) => {
+        try {
+            const {email, password } = req.body;
 
-        const db = client.db('SalvageFinancialDB');
-        const usersCollection = db.collection('Users');
-        
-        const users = await usersCollection.findOne(
-            { email: email, password: password }, // Search criteria
-            { projection: { _id: 1 } } // Return only the _id field
-    	);
-        res.status(200).json(users, {error: error});
-    } catch (error) {
-        console.error("❌ Fetch Error:", error);
-        res.status(500).json({ error: "Could not sign in" });
-    }
-});
+            const db = client.db('SalvageFinancialDB');
+            const usersCollection = db.collection('Users');
+            
+            const users = await usersCollection.findOne(
+                { email: email, password: password }, // Search criteria
+                { projection: { _id: 1 } } // Return only the _id field
+            );
+            res.status(200).json(users, {error: error});
+        } catch (error) {
+            console.error("❌ Fetch Error:", error);
+            res.status(500).json({ error: "Could not sign in" });
+        }
+    });
 
-app.get('/api/signup', async (req, res) => {
-    try {
-		const { fname, lname, email, password } = req.body;
-    	const newUser = {fname: fname, lname: lname, email: email, password: password};
-		var error = 'Finished succesfully';
+    app.get('/api/signup', async (req, res) => {
+        try {
+            const { fname, lname, email, password } = req.body;
+            const newUser = {fname: fname, lname: lname, email: email, password: password};
+            var error = 'Finished succesfully';
 
-        const db = client.db('SalvageFinancialDB');
-        const usersCollection = db.collection('Users');
-        
-        const users = await usersCollection.insertOne(newUser);
-        res.status(200).json({error: error});
-    } catch (error) {
-        console.error("❌ Fetch Error:", error);
-        res.status(500).json({ error: "Could not sign up" });
-    }
-});
+            const db = client.db('SalvageFinancialDB');
+            const usersCollection = db.collection('Users');
+            
+            const users = await usersCollection.insertOne(newUser);
+            res.status(200).json({error: error});
+        } catch (error) {
+            console.error("❌ Fetch Error:", error);
+            res.status(500).json({ error: "Could not sign up" });
+        }
+    });
 
-/*app.get('/api/users', async (req, res) => {
-    try {
-        const db = client.db('SalvageFinancialDB');
-        const usersCollection = db.collection('Users');
-        
-        const users = await usersCollection.find().toArray();
-        res.status(200).json(users);
-    } catch (error) {
-        console.error("❌ Fetch Error:", error);
-        res.status(500).json({ error: "Could not fetch users" });
-    }
-});*/
-
+    /*app.get('/api/users', async (req, res) => {
+        try {
+            const db = client.db('SalvageFinancialDB');
+            const usersCollection = db.collection('Users');
+            
+            const users = await usersCollection.find().toArray();
+            res.status(200).json(users);
+        } catch (error) {
+            console.error("❌ Fetch Error:", error);
+            res.status(500).json({ error: "Could not fetch users" });
+        }
+    });*/
+}
+/*
 app.listen(port, () => {
     console.log(`🚀 Server running on http://localhost:${port}`);
-});
+});*/
--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-require('dotenv').config()
-const url = process.env.MONODB_URI;
+//require('dotenv').config()
+//const url = process.env.MONODB_URI;
+const url = 'mongodb+srv://kentaf1202:Lihys2G76A1hS6Ld@salvagefinancialdb.pvcx6.mongodb.net/SalvageFinancialDB?retryWrites=true&w=majority';
 var api = require('./api.js');
 api.setApp(app, client);
 
