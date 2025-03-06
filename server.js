429d30640ce62b1cbed957c30116277121c8e476
KentaF1202 <kentaf1202@gmail.com>
2025-03-06 17:39:50 -0500

    add client to server.js

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-//require('dotenv').config()
-//const url = process.env.MONODB_URI;
-const url = 'mongodb+srv://kentaf1202:Lihys2G76A1hS6Ld@salvagefinancialdb.pvcx6.mongodb.net/SalvageFinancialDB?retryWrites=true&w=majority';
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
+//require('dotenv').config()
+//const url = process.env.MONODB_URI;
+const MongoClient = require('mongodb').MongoClient; 
+const url = 'mongodb+srv://kentaf1202:Lihys2G76A1hS6Ld@salvagefinancialdb.pvcx6.mongodb.net/SalvageFinancialDB?retryWrites=true&w=majority';
+const client = new MongoClient(url);
+client.connect();
+
+var api = require('./api.js');
+api.setApp(app, client);
 
 app.use((req, res, next) => {
 	res.setHeader('Access-Control-Allow-Origin', '*');
@@ -17,8 +23,5 @@ app.use((req, res, next) => {
 		'GET, POST, PATCH, DELETE, OPTIONS');
 	next();
 });
-
-var api = require('./api.js');
-api.setApp(app, client);
 
 app.listen(5000);
