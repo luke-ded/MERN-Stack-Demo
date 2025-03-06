b9a57bffa6814c1d905b30a84bb01eac6da137f6
KentaF1202 <kentaf1202@gmail.com>
2025-03-06 17:20:18 -0500

    Change var api location

--- server.js
+++ server.js
@@ -4,9 +4,6 @@ const cors = require('cors');
 //require('dotenv').config()
 //const url = process.env.MONODB_URI;
 const url = 'mongodb+srv://kentaf1202:Lihys2G76A1hS6Ld@salvagefinancialdb.pvcx6.mongodb.net/SalvageFinancialDB?retryWrites=true&w=majority';
-var api = require('./api.js');
-api.setApp(app, client);
-
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
@@ -20,5 +17,8 @@ app.use((req, res, next) => {
 		'GET, POST, PATCH, DELETE, OPTIONS');
 	next();
 });
+
+var api = require('./api.js');
+api.setApp(app, client);
 
 app.listen(5000);
