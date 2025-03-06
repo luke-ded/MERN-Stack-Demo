a6d2a66fbee07eb49848439acee6612b07da1bc9
KentaF1202 <kentaf1202@gmail.com>
2025-03-06 18:03:35 -0500

    bug fix api

--- api.js
+++ api.js
@@ -1,19 +1,5 @@
 require('express');
 require('mongodb');
-/*const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-
-const app = express();
-app.use(cors());
-app.use(bodyParser.json());
-const port = 5000;
-
-const MongoClient = require('mongodb').MongoClient; 
-const url = 'mongodb+srv://kentaf1202:Lihys2G76A1hS6Ld@salvagefinancialdb.pvcx6.mongodb.net/SalvageFinancialDB?retryWrites=true&w=majority';
-
-const client = new MongoClient(url);
-client.connect();*/
 
 exports.setApp = function ( app, client )
 {
@@ -28,7 +14,8 @@ exports.setApp = function ( app, client )
                 { email: email, password: password }, // Search criteria
                 { projection: { _id: 1 } } // Return only the _id field
             );
-            res.status(200).json(users, {error: error});
+            const error = "Could not find user";
+            res.status(200).json(users || {error: error});
         } catch (error) {
             console.error("❌ Fetch Error:", error);
             res.status(500).json({ error: "Could not sign in" });
@@ -51,21 +38,8 @@ exports.setApp = function ( app, client )
             res.status(500).json({ error: "Could not sign up" });
         }
     });
-
-    /*app.get('/api/users', async (req, res) => {
-        try {
-            const db = client.db('SalvageFinancialDB');
-            const usersCollection = db.collection('Users');
-            
-            const users = await usersCollection.find().toArray();
-            res.status(200).json(users);
-        } catch (error) {
-            console.error("❌ Fetch Error:", error);
-            res.status(500).json({ error: "Could not fetch users" });
-        }
-    });*/
 }
 /*
-app.listen(port, () => {
+app.listen(5000, () => {
     console.log(`🚀 Server running on http://localhost:${port}`);
 });*/
