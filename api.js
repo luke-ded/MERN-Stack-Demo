741f6018fa52cbb8b87beef14b911024d30a3c93
KentaF1202 <kentaf1202@gmail.com>
2025-03-06 17:43:27 -0500

    api.js change

--- api.js
+++ api.js
@@ -17,21 +17,6 @@ client.connect();*/
 
 exports.setApp = function ( app, client )
 {
-    app.use((req, res, next) =>
-    {
-        res.setHeader('Access-Control-Allow-Origin', '*');
-        res.setHeader(
-        'Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-        );
-        res.setHeader(
-        'Access-Control-Allow-Methods',
-        'GET, POST, PATCH, DELETE, OPTIONS'
-        );
-
-        next();
-    });
-
     app.get('/api/login', async (req, res) => {
         try {
             const {email, password } = req.body;
