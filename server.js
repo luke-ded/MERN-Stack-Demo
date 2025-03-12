ca45520f2222b8a8d5ca3d6d2ee32fb1b48c0a7a
luke-ded <luke@dederich.org>
2025-03-12 09:08:50 -0400

    Login api test server.js

--- server.js
+++ server.js
@@ -30,12 +30,12 @@ app.use((req, res, next) =>
 {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader(
-	'Access-Control-Allow-Headers',
-	'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
     );
     res.setHeader(
-	'Access-Control-Allow-Methods',
-	'GET, POST, PATCH, DELETE, OPTIONS'
+    'Access-Control-Allow-Methods',
+    'GET, POST, PATCH, DELETE, OPTIONS'
     );
     next();
 });
@@ -43,6 +43,37 @@ app.use((req, res, next) =>
 //API calls
 var api = require('./api.js');
 api.setApp(app, client);
+
+
+app.post('/api/login', async (req, res, next) =>
+{
+    try
+    {
+        // incoming: login, password
+        // outgoing: id, firstName, lastName, error
+        var error = '';
+        const { login, password } = req.body;
+        const db = client.db('SalvageFinancialDB');
+        const results = await
+        db.collection('Users').find({Login:login,Password:password}).toArray();
+        var id = -1;
+        var fn = '';
+        var ln = '';
+        if( results.length > 0 )
+        {
+            id = results[0].UserId;
+            fn = results[0].FirstName; 
+            ln = results[0].LastName;
+        }
+        var ret = { id:id, firstName:fn, lastName:ln, error:''};
+        res.status(200).json(ret);
+    } 
+    catch (error)
+    {
+        console.error("❌ Fetch Error:", error);
+        res.status(500).json({ error: "Could not fetch users" });
+    }
+});
 
 //Listening on port 5000
 app.listen(5000);
