aff547a4c3475869a6ad1581055a70ae54b3d538
Dakshish Sannala <duckcheese23@gmail.com>
2025-03-13 21:18:46 -0400

    Added api link for resetPass

--- frontend/src/components/ResetPass.tsx
+++ frontend/src/components/ResetPass.tsx
@@ -4,6 +4,47 @@ import app from "../pages/App.module.css";
 
 function ResetPass(){
 
+    async function resetPassword(event: any) : Promise<void>
+    {
+        const userEmail = localStorage.getItem("userEmail");
+        const newPassword = (document.getElementById("ConfirmReset") as HTMLInputElement).value;
+        const alertMessage = document.getElementById("alertMessage");
+
+        if (alertMessage) {
+            var email = userEmail;
+            var newPass = newPassword;
+            event.preventDefault();
+            var obj = {Email:userEmail,Password:newPass};
+            var js = JSON.stringify(obj);
+
+            try
+            {
+                const response = await fetch('http://salvagefinancial.xyz:5000/api/ResetPassword',
+                {method:'POST',body:js,headers:{'Content-Type':'application/json'}});
+                var res = JSON.parse(await response.text());
+                if( res._id <= 0 )
+                {
+                    alertMessage.innerText = 'Failed to reset password';
+                    alertMessage.style.visibility = "visible"; 
+                }
+                else
+                {
+                    alertMessage.innerText = 'Successfully changed password, you can close this tab';
+                    localStorage.setItem('user_data', JSON.stringify(res));
+            
+
+            
+                }
+            } 
+            catch(error:any)
+            {
+                alert(error.toString());
+                return;
+            }
+
+        }
+    }
+
     return(
         <div id ="ResetDiv">
             <span id="inner-title">Reset Password</span><br />
@@ -11,8 +52,8 @@ function ResetPass(){
             <input type="text" id="FirstReset" className = {app.logininputs} placeholder="Password"/><br/>
             <h5 className={app.loginlabel}>Confirm Password</h5>
             <input type="text" id="ConfirmReset" className = {app.logininputs} placeholder="Confirm Password"/><br/>
-            <button className={app.loginbuttons} id={app.dologinbutton}>Change Password</button>
             <h5 id = "alertMessage"></h5>
+            <button className={app.loginbuttons} id={app.dologinbutton} onClick={resetPassword}>Change Password</button>
         </div>
     );
 
