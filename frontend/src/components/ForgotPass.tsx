515b6532242c4a48e86fef755e0c32d441aef677
Dakshish Sannala <duckcheese23@gmail.com>
2025-03-20 14:07:40 -0400

    import usestate to forgotpass

--- frontend/src/components/ForgotPass.tsx
+++ frontend/src/components/ForgotPass.tsx
@@ -1,6 +1,7 @@
 import app from "../pages/App.module.css";
 import { useNavigate } from 'react-router-dom';
 import emailjs from 'emailjs-com';
+import {useState} from 'react'
 
 
 function ForgotPass(){
