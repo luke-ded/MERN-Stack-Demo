07c3c48c64ecf13b6d5e03470cb7b077cc6040aa
luke-ded <luke@dederich.org>
2025-03-28 14:03:07 -0400

    Dashboard statistics & improvements

--- frontend/src/pages/DashboardPage.tsx
+++ frontend/src/pages/DashboardPage.tsx
@@ -3,16 +3,15 @@ import GoalProgress from '../components/dashboardcomponents/GoalProgress.tsx';
 import NavBar from '../components/NavBar.tsx';
 
 
-// Luke is working on this page, please DO NOT edit!!!!!!!!!!!!!!!!!!!!!!!!!!!
 const DashboardPage = () => {
     const percentage = 60;
     const rewardName = "rewardName";
 
     return (
-    <div className="flex flex-col absolute top-0 left-0 h-screen w-screen bg-yellow-400/50">
+    <div className="flex flex-col absolute top-0 left-0 h-screen w-screen">
         <NavBar />
 
-        <div className="flex justify-start mt-[8vh] bg-red-400/50">
+        <div className="flex justify-start mt-[8vh]">
             <div className= "ml-8 w-[50vh] min-h-fit h-[50vh] border border-[#6d91e8] rounded-[2%] text-center bg-[rgba(17,18,23,.9)]">
         <       h3 className="font-[Lucida Sans] font-bold text-2xl text-[#6d91e8] mt-3">Expenses</h3>
                 <DoughnutChart />
@@ -22,25 +21,40 @@ const DashboardPage = () => {
             <h3 className="font-[Lucida Sans] font-bold text-2xl text-[#6d91e8] mt-3">Income</h3>
                 <DoughnutChart />
             </div>
+
+            <div className= "ml-8 w-[24.5vw] min-h-[50vh] h-[50vh] border border-[#6d91e8] rounded-[2%] text-center bg-[rgba(17,18,23,.9)]">
+            <h3 className="font-[Lucida Sans] font-bold text-2xl text-[#6d91e8] mt-3">Statistics</h3>
+
+            <div className="mt-4 ml-4 text-left font-[Lucida Sans]">
+                <p className="text-[#ff6384]">Total Debt:</p>
+                <p className="text-[#36eba6]">Total savings: </p>
+                <p>Spending this month:</p>
+                <p>% spending on essentials: </p>
+                <p>Need more ideas for this: </p>
+            </div>
+            
+                
+            </div>
         </div>
 
         {/* This is super rough */}
         <div className= "mt-8 ml-8 w-[50vh] min-h-fit h-[50vh] border border-[#6d91e8] rounded-[2%] text-center bg-[rgba(17,18,23,.9)]">
-            <div className="flex w-[50vh%] bg-orange-400/50 absolute z-0">
+            <div className="flex w-[50vh%] absolute z-0">
                 <GoalProgress />
                 
             </div>
             <h3 className="font-[Lucida Sans] font-bold text-2xl text-[#6d91e8] mt-3">Reward</h3>
             
-            <div >
+            <div className='mt-10'>
                 <h5 style={{color: percentage > 50 ? "rgb(54, 235, 166)" : "rgb(255, 99, 132)"}}> 
                     You are {percentage}% of the way to your reward, {rewardName}!</h5>
             </div>
         </div>
 
-        {/* <div className={dashapp.dashboardDiv} id={dashapp.depthmeter}>
-            <h3 id={dashapp.chartlabel}>Depth</h3>
-        </div> */}
+        <div className="w-[10vw] min-h-fit h-[90vh] border border-[#6d91e8] rounded-lg text-center bg-[rgba(17,18,23,.9)] fixed top-[8vh] right-8">
+            <h3 className="font-[Lucida Sans] font-bold text-2xl text-[#6d91e8] mt-3">Depth</h3>
+            <p className="mt-10 m-2">(replace this with a graphic designed depth meter)</p>
+        </div>
         
     </div>
     );
