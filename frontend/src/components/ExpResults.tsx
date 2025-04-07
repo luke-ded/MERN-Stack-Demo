41e36e6f0b4986252fb8edc92df1b2a2807e3c2c
Dakshish Sannala <duckcheese23@gmail.com>
2025-04-06 23:41:08 -0400

    bug fix

--- frontend/src/components/ExpResults.tsx
+++ frontend/src/components/ExpResults.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-
+import ReactDOM from 'react-dom/client';
 
 interface Item 
 {
@@ -36,6 +36,8 @@ function refreshExpenseList() {
         
     ));
 
+
+    ReactDOM.createRoot(container).render(<>{listItems}</>);
    
     return;
 }
