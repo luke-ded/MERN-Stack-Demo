3af7fb453b5cb2565f3453504fe6ebba18f074de
Dakshish Sannala <duckcheese23@gmail.com>
2025-04-07 15:43:41 -0400

    Removed red exit buttons on financialspage

--- frontend/src/pages/FinancialsPage.tsx
+++ frontend/src/pages/FinancialsPage.tsx
@@ -3,146 +3,11 @@ import NavBar from '../components/NavBar.tsx';
 import VisualFinances from '../components/VisualFinances.tsx';
 import Results from '../components/Results.tsx';
 import Expresults from '../components/ExpResults.tsx';
-import app from './App.module.css';
-import {useState} from 'react'
+
 
 const FinancialsPage = () => {
 
-  const [isFinanceBarClosed, setFinanceBar] = useState(false);
-  const [isVisualBarClosed, setVisualBar] = useState(false);
-  const [isResultBarClosed, setResultBar] = useState(false);
-  const [isExpenBarClosed, setExpenBar] = useState(false);
-
-  function closeFinanceBar(){ 
-    const bar = document.getElementById("FinanceBar");
-    const finances = document.getElementById("finances");
-    const btn = document.getElementById(app.exitBtn);
-    const alertMessage = document.getElementById("alertmessage");
-
-    if (bar && finances && btn && alertMessage){
-      alertMessage.style.visibility = "hidden";
-      finances.style.visibility = "hidden";
-      bar.style.backgroundColor = "rgba(0,26, 51)";
-      bar.style.border = "none";
-      btn.innerHTML = "&#10010;"
-      setFinanceBar(true);
-    }
-
-  }
-
-  function openFianceBar(){
-    const bar = document.getElementById("FinanceBar");
-    const finances = document.getElementById("finances");
-    const btn = document.getElementById(app.exitBtn);
-
-    if (bar && finances && btn){
-      finances.style.visibility = "visible";
-      bar.style.backgroundColor = "rgba(17,18,23, .9)";
-      bar.style.border = "1.5px solid #6d91e8";
-      btn.innerHTML = "&#10060;"
-      setFinanceBar(false);
-    }
-  }
-
-
-  function closeVisualBar(){
-
-    const bar = document.getElementById("VisualBar");
-    const visual = document.getElementById("visual");
-    const btn = document.getElementById(app.exitBtnV);
-    const alertMessage = document.getElementById("alertMessage");
-
-    if (bar && visual && btn && alertMessage){ 
-      alertMessage.style.visibility = "hidden";   
-      visual.style.visibility = "hidden";
-      bar.style.backgroundColor = "rgba(0,26, 51)";
-      bar.style.border = "none";
-      btn.innerHTML = "&#10010;"
-      setVisualBar(true);
-    }
-
-  }
-
-  function openVisualBar(){
-
-    const bar = document.getElementById("VisualBar");
-    const visual = document.getElementById("visual");
-    const btn = document.getElementById(app.exitBtnV);
-
-    if (bar && visual && btn){  
-      visual.style.visibility = "visible";
-      bar.style.backgroundColor = "rgba(17,18,23, .9)";
-      bar.style.border = "1.5px solid #6d91e8";
-      btn.innerHTML = "&#10060;"
-      setVisualBar(false);
-    }
-
-  }
-
-  function openResultBar(){
-
-    const bar = document.getElementById("ResultsBar");
-    const result = document.getElementById("results");
-    const btn = document.getElementById(app.exitBtnR);
-
-    if (bar && result && btn){  
-      result.style.visibility = "visible";
-      bar.style.backgroundColor = "rgba(17,18,23, .9)";
-      bar.style.border = "1.5px solid #6d91e8";
-      btn.innerHTML = "&#10060;"
-      setResultBar(false);
-    }
-
-  }
-
-  function closeResultBar(){
-
-    const bar = document.getElementById("ResultsBar");
-    const result = document.getElementById("results");
-    const btn = document.getElementById(app.exitBtnR);
-    
-
-    if (bar && result && btn){  
-      result.style.visibility = "hidden";
-      bar.style.backgroundColor = "rgba(0,26, 51)";
-      bar.style.border = "none";
-      btn.innerHTML = "&#10010;"
-      setResultBar(true);
-    }
-  }
-
-  function openExpBar(){
-
-    const bar = document.getElementById("ExpResultsBar");
-    const result = document.getElementById("ExpRes");
-    const btn = document.getElementById(app.exitBtnE);
-
-    if (bar && result && btn){  
-      result.style.visibility = "visible";
-      bar.style.backgroundColor = "rgba(17,18,23, .9)";
-      bar.style.border = "1.5px solid #6d91e8";
-      btn.innerHTML = "&#10060;"
-      setExpenBar(false);
-    }
-
-  }
-
-  function closeExpBar(){
-
-    const bar = document.getElementById("ExpResultsBar");
-    const result = document.getElementById("ExpRes");
-    const btn = document.getElementById(app.exitBtnE);
-    
-
-    if (bar && result && btn){  
-      result.style.visibility = "hidden";
-      bar.style.backgroundColor = "rgba(0,26, 51)";
-      bar.style.border = "none";
-      btn.innerHTML = "&#10010;"
-      setExpenBar(true);
-    }
-  }
-
+  
 
 
   return (
@@ -151,22 +16,18 @@ const FinancialsPage = () => {
 
          <div className = "block absolute top-2/7 left-2/7 w-[30%] h-[40%] min-h-fit bg-[rgba(17,18,23,0.9)] border border-[#6d91e8] border-[1.5px] rounded-[2%] text-center transform -translate-x-1/2 -translate-y-1/2" id ="FinanceBar">
           <Finances />
-          <button id = {app.exitBtn} onClick={() => {if(isFinanceBarClosed == false) {closeFinanceBar();} else {openFianceBar();}}}>&#10060;</button><br />
         </div>
 
         <div className = "block absolute top-[30%] left-[70%] w-[30%] h-[45%] min-h-fit bg-[rgba(17,18,23,0.9)] border border-[#6d91e8] border-[1.5px] rounded-[2%] text-center transform -translate-x-1/2 -translate-y-1/2" id = "VisualBar">
           <VisualFinances />
-          <button id = {app.exitBtnV} onClick = {() => {if(isVisualBarClosed == false) {closeVisualBar();} else {openVisualBar();}}}>&#10060;</button><br />
         </div>
 
         <div className = "block absolute top-3/4 left-2/7 w-[30%] h-[40%] min-h-fit bg-[rgba(17,18,23,0.9)] border border-[#6d91e8] border-[1.5px] rounded-[2%] text-center transform -translate-x-1/2 -translate-y-1/2" id = "ResultsBar">
           <Results />
-          <button id = {app.exitBtnR} onClick = {() => {if(isResultBarClosed == false) {closeResultBar();} else {openResultBar();}}}>&#10060;</button><br />
         </div>
 
         <div className = "block absolute top-[76%] left-[70%] w-[30%] h-[43%] min-h-fit bg-[rgba(17,18,23,0.9)] border border-[#6d91e8] border-[1.5px] rounded-[2%] text-center transform -translate-x-1/2 -translate-y-1/2" id = "ExpResultsBar">
             <Expresults />
-            <button id = {app.exitBtnE} onClick = {() => {if(isExpenBarClosed == false) {closeExpBar();} else {openExpBar();}}}>&#10060;</button><br />
         </div>
     </div>
   );
