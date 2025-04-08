86b9faabf109bd828b56a0ab0ef5c02964a1652e
Dakshish Sannala <duckcheese23@gmail.com>
2025-04-08 11:07:29 -0400

    Making Expenses UI slightly bigger

--- frontend/src/pages/FinancialsPage.tsx
+++ frontend/src/pages/FinancialsPage.tsx
@@ -26,7 +26,7 @@ const FinancialsPage = () => {
           <Results />
         </div>
 
-        <div className = "block absolute top-[76%] left-[70%] w-[30%] h-[43%] min-h-fit bg-[rgba(17,18,23,0.9)] border border-[#6d91e8] border-[1.5px] rounded-[2%] text-center transform -translate-x-1/2 -translate-y-1/2" id = "ExpResultsBar">
+        <div className = "block absolute top-[76%] left-[70%] w-[30%] h-[44%] min-h-fit bg-[rgba(17,18,23,0.9)] border border-[#6d91e8] border-[1.5px] rounded-[2%] text-center transform -translate-x-1/2 -translate-y-1/2" id = "ExpResultsBar">
             <Expresults />
         </div>
     </div>
