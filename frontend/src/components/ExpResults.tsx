a52e3f7e7135742a3e86ac1cb67d0b0571274ffc
Dakshish Sannala <duckcheese23@gmail.com>
2025-04-08 17:45:42 -0400

    Quick change to expResults

--- frontend/src/components/ExpResults.tsx
+++ frontend/src/components/ExpResults.tsx
@@ -359,7 +359,7 @@ function ExpResults()
 
     return(
 
-        <div id = "ExpRes">
+        <div id = "ExpRes" className="flex flex-col h-full">
 
 
             <div className="flex h-[10%] items-center justify-center border-b border-[#6d91e8]">
@@ -368,8 +368,8 @@ function ExpResults()
 
             </div>
 
-            <div className = "rounded-lg grow min-h-0">
-                <ul className="flex-none overflow-y-scroll shadow divide-y divide-[#7f8fb5] border-b border-[#6d91e8] px-4 py-2 h-[300px]" id = "listss"> 
+            <div className = "flex flex-col flex-grow min-h-0">
+                <ul className="flex-grow overflow-y-auto shadow divide-y divide-[#7f8fb5] border-b border-[#6d91e8] px-4 py-2" id = "listss"> 
                     {props.items.map((item) => {
                     return <li className="px-[1vw] py-[1vh] hover:bg-white/5">{props.renderer(item)}</li>;
                     })}
