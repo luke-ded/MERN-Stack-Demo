65b7d231dedb4a5a980eb1aecbb71acdcf1364f4
Dakshish Sannala <duckcheese23@gmail.com>
2025-04-08 19:18:34 -0400

    Minor bug fix

--- frontend/src/components/ExpResults.tsx
+++ frontend/src/components/ExpResults.tsx
@@ -91,7 +91,7 @@ function displayEModal(item: Item) {
                 <input className="h-6 w-8/10 text-lg rounded-sm border border-[#6d91e8] bg-blue-400/5 focus:outline-none p-1" type="text" placeholder = {date} id = "Expdate"></input>
 
                 <h5 className="self-start ml-[10%] text-lg text-left text-[0.95rem]">Category</h5>
-                <input className="h-6 w-8/10 text-lg rounded-sm border border-[#6d91e8] bg-blue-400/5 focus:outline-none p-1" type="text" placeholder = "Category" id = "ExpCat"></input>
+                <input className="h-6 w-8/10 text-lg rounded-sm border border-[#6d91e8] bg-blue-400/5 focus:outline-none p-1" type="text" placeholder = {item.Category} id = "ExpCat"></input>
 
                 <h5 className="self-start ml-[10%] text-lg text-left text-[0.95rem] mt-2">Is The Expense Recurring?</h5>
 
