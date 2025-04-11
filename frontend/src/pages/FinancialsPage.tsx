16527dd76d1e94f40ee992add60d70300e42b02d
Dakshish Sannala <duckcheese23@gmail.com>
2025-04-10 20:54:12 -0400

    Fixed delete-add-delete bug for income

--- frontend/src/pages/FinancialsPage.tsx
+++ frontend/src/pages/FinancialsPage.tsx
@@ -26,7 +26,7 @@ const FinancialsPage = () => {
         </div>
 
         <div className = "block fixed top-[76%] left-2/7 w-[30%] h-[40%] min-h-fit bg-[rgba(17,18,23,0.9)] border border-[#6d91e8] border-[1.5px] rounded-[2%] text-center transform -translate-x-1/2 -translate-y-1/2" id = "ResultsBar">
-          <Results />
+          <Results triggerRerender={triggerRerender}/>
         </div>
 
         <div className = "block fixed top-[76%] left-[70%] w-[30%] h-[40%] min-h-fit bg-[rgba(17,18,23,0.9)] border border-[#6d91e8] border-[1.5px] rounded-[2%] text-center transform -translate-x-1/2 -translate-y-1/2" id = "ExpResultsBar">
