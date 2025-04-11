adb2e99b6cad891461f0a932a7ee07f8b1b539ab
Dakshish Sannala <duckcheese23@gmail.com>
2025-04-10 22:03:30 -0400

    TriggerRenderer bug for debt and savings page

--- frontend/src/pages/DebtPage.tsx
+++ frontend/src/pages/DebtPage.tsx
@@ -31,7 +31,7 @@ const FinancialsPage = () => {
 
         
         <div className = "block absolute top-[76%] left-[70%] w-[30%] h-[44%] min-h-fit bg-[rgba(17,18,23,0.9)] border border-[#6d91e8] border-[1.5px] rounded-[2%] text-center transform -translate-x-1/2 -translate-y-1/2" id = "ExpResultsBar">
-            <Expresults />
+            <Expresults triggerRerender={triggerRerender}/>
         </div>
     </div>
   );
