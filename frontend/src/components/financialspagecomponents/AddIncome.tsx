c289872c84528c701d8810e0e21654acb833b152
luke-ded <luke@dederich.org>
2025-04-12 10:02:26 -0400

    Add income account integration

--- frontend/src/components/financialspagecomponents/AddIncome.tsx
+++ frontend/src/components/financialspagecomponents/AddIncome.tsx
@@ -7,10 +7,35 @@ interface ChildProps {
     triggerRerender: () => void;
 }
 
+interface Item 
+{
+    key:string;
+    Name: string;
+    Date: any;
+    Amount: any;
+    APR: any;
+}
+
 const AddIncome: React.FC<ChildProps> = ({ triggerRerender }) => {
 
     const [isRecurring, setRecurring] = useState(false);
     const [isButtonClicked, setButton] = useState(false);
+    const [selectedItemObject, setSelectedItemObject] = useState<Item | null>(null);
+    const [selectedItemKey, setSelectedItemKey] = useState<string>('');
+
+    const itemsList = setSavings();
+
+    const handleItemChange = (event: React.ChangeEvent<HTMLSelectElement>) => 
+    {
+        const selectedKey = event.target.value;
+        setSelectedItemKey(selectedKey);
+
+        const fullItem = itemsList.find(item => item.key === selectedKey) || null;
+        setSelectedItemObject(fullItem);
+        console.log("Selected Item Key:", selectedKey);
+        console.log("Selected Item Object:", fullItem);
+    };
+
 
     function setYes(){
         setRecurring(true);
@@ -22,6 +47,107 @@ const AddIncome: React.FC<ChildProps> = ({ triggerRerender }) => {
         setButton(true);
     }
 
+    function setSavings()
+    {
+        const today = new Date();
+    
+        var data = localStorage.getItem('user_data');
+        var parsedData = data ? JSON.parse(data) : null;
+    
+        let savings = new Array<Item>();
+
+        for(var i = 0; i < parsedData.User.Savings.length; i++) 
+        {
+            var counter = parsedData.User.Savings[i];
+    
+            // Ensures item is not in the future
+            if(counter.InitialTime != undefined)
+            {
+                let old = new Date(Date.UTC(counter.InitialTime.Year, counter.InitialTime.Month - 1, counter.InitialTime.Day));
+                if((today.getTime() - old.getTime()) < 0)
+                    continue;
+            }
+    
+            let newItem: Item = 
+            {
+                key: i.toString(),
+                Name: counter.Name, 
+                Date: counter.InitialTime != undefined ? counter.InitialTime : {"Month":1, "Day":1, "Year":2023},
+                Amount: counter.Amount,
+                APR: counter.APR,
+            };
+    
+            savings.push(newItem);
+        }
+        
+        // Add "account" for when going to cash
+        let newItem: Item = 
+        {
+            key: parsedData.User.Savings.Length,
+            Name: "Untracked", 
+            Date: {"Month":1, "Day":1, "Year":2023},
+            Amount: 0,
+            APR: 0,
+        };
+    
+        savings.push(newItem);
+
+        return savings;
+    }
+
+
+    async function addFunds(item: Item | null, amount : any) 
+    {
+        var data = localStorage.getItem('user_data');
+        var parsedData = data ? JSON.parse(data) : null;
+
+        if(item == null)
+            return;
+
+        if(item.key == parsedData.User.Savings.Length)
+            return;
+
+
+        const token = localStorage.getItem('token');
+
+        var index = parseInt(item.key);
+
+        var obj = 
+        {
+            index: index,
+            NewName: item.Name,
+            NewAmount: item.Amount + amount, // Only thing updated here
+            NewAPR: item.APR,
+            NewInitialTime: item.Date
+        };
+
+        console.log(obj);
+        var js = JSON.stringify(obj);
+
+        try 
+        {
+            const response = await fetch('http://salvagefinancial.xyz:5000/api/EditSaving',
+                { method:'POST', body:js, headers:{'Content-Type':'application/json', 'Authorization': `Bearer ${token}`}});
+            var res = JSON.parse(await response.text());
+
+            if (res.Result === "Edited saving of user") 
+            {
+                console.log("edited\n");
+                await updateInfo();
+                //triggerRerender(); // Ensure parent knows data changed
+            } 
+            else 
+            {
+                console.error("Edit failed:", res.Result);
+                // Consider adding user feedback here
+            }
+        } 
+        catch (error: any) 
+        {
+            alert(error.toString());
+        }
+    }
+
     async function addIncome(event: any): Promise<void>{
 
         const valAmount = (document.getElementById("IncNum") as HTMLInputElement).value;
@@ -30,7 +156,7 @@ const AddIncome: React.FC<ChildProps> = ({ triggerRerender }) => {
         const alertMessage = document.getElementById("alertmessage");
 
         if (alertMessage) {
-            if (date.length == 0 || userName.length == 0 || valAmount.length == 0 || isButtonClicked == false){
+            if (date.length == 0 || userName.length == 0 || valAmount.length == 0 || isButtonClicked == false || selectedItemKey == ''){
                 alertMessage.innerText = "Please Complete all the fields";
                 alertMessage.style.color = "#ff6384";
                 alertMessage.style.visibility = "visible";
@@ -82,6 +208,7 @@ const AddIncome: React.FC<ChildProps> = ({ triggerRerender }) => {
                     alertMessage.style.color = "#00c04b";
                     alertMessage.style.visibility = "visible";
 
+                    addFunds(selectedItemObject, Amount);
                     await updateInfo();
                     triggerRerender();
                 }
@@ -162,7 +289,38 @@ const AddIncome: React.FC<ChildProps> = ({ triggerRerender }) => {
                 
 
                 <h5 className="self-start ml-[10%] text-lg text-left text-[0.95rem]">Date</h5>
-                <input className="h-1/2 w-8/10 text-lg rounded-sm border border-[#6d91e8] bg-blue-400/5 focus:outline-none focus:ring-1 focus:ring-[#7f8fb5] p-1" type="text" placeholder = "MM/DD/YYYY" id = "Incdate"></input>
+                <input className="h-1/2 w-8/10 text-lg rounded-sm border border-[#6d91e8] bg-blue-400/5 focus:outline-none focus:ring-1 focus:ring-[#7f8fb5] p-1" type="text" placeholder = "MM/DD/YYYY" id = "Incdate" onKeyUp={(e) => {
+                    if (e.key === "Enter") 
+                    {
+                      var next = document.getElementById("Incaccount") as HTMLInputElement;
+                      next.focus();
+                    }
+                  }}></input>
+
+                <h5 className="self-start ml-[10%] text-lg text-left text-[0.95rem]">Account</h5>
+
+                <select
+                    id="Incaccount"
+                    value={selectedItemKey} // Controlled component: value linked to state
+                    onChange={handleItemChange} // Update state on change
+                    className="h-[1/2] w-8/10 text-lg rounded-sm border border-[#6d91e8] bg-blue-400/5 focus:outline-none focus:ring-1 focus:ring-[#7f8fb5] p-1"
+                    required
+                    >
+
+                    {/* Placeholder Option */}
+                    <option className="text-black bg-[#7f8fb5]" value="" disabled>
+                      Select Account
+                    </option>
+
+                    {itemsList.map((item) => (
+                      <option className="text-black bg-[#7f8fb5]"
+                        key={item.key}
+                        value={item.key}
+                      >
+                        {item.Name} {/* Display the item's Name to the user */}
+                      </option>
+                    ))}
+                </select>
 
                 <div className="flex items-center justify-between ml-[10%] mt-2 w-8/10">
                     <h5 className="self-start text-lg text-left text-[0.95rem]">Is The Income Recurring?</h5> 
@@ -178,7 +336,7 @@ const AddIncome: React.FC<ChildProps> = ({ triggerRerender }) => {
                     </div>
                 </div>
 
-                <div className="fixed top-[82%] w-[100%] flex items-center justify-between">
+                <div className="fixed top-[85%] w-[100%] flex items-center justify-between">
                     <h5 className="ml-[10%] text-[#ff6384]" id="alertmessage"></h5>
                     <button id = "AddIncome" className = "rounded-sm inline-block h-fit w-fit mr-[10%] p-[10px] pt-[5px] pb-[7px] bg-transparent border border-[#6d91e8] text-center text-[1.8vh] hover:bg-blue-400/15 hover:border-[#bdc8e2]" onClick = {addIncome} >Add Income</button>
                 </div>
--- frontend/src/pages/FinancialsPage.tsx
+++ frontend/src/pages/FinancialsPage.tsx
@@ -17,19 +17,19 @@ const FinancialsPage = () => {
     <div className = "absolute top-0 left-0 h-full w-full bg-[rgba(0,26,51,1)]">
          <NavBar />
 
-         <div className = "block fixed top-[28%] left-2/7 w-[30%] h-[41%] min-h-fit bg-[rgba(17,18,23,0.9)] border border-[#6d91e8] border-[1.5px] rounded-[2%] text-center transform -translate-x-1/2 -translate-y-1/2" id ="FinanceBar">
+         <div className = "block fixed top-[32%] left-2/7 w-[30%] h-[48%] min-h-fit bg-[rgba(17,18,23,0.9)] border border-[#6d91e8] border-[1.5px] rounded-[2%] text-center transform -translate-x-1/2 -translate-y-1/2" id ="FinanceBar">
           <AddIncome triggerRerender={triggerRerender}/>
         </div>
 
-        <div className = "block fixed top-[29.5%] left-[70%] w-[30%] h-[44%] min-h-fit bg-[rgba(17,18,23,0.9)] border border-[#6d91e8] border-[1.5px] rounded-[2%] text-center transform -translate-x-1/2 -translate-y-1/2" id = "VisualBar">
+        <div className = "block fixed top-[32%] left-[70%] w-[30%] h-[48%] min-h-fit bg-[rgba(17,18,23,0.9)] border border-[#6d91e8] border-[1.5px] rounded-[2%] text-center transform -translate-x-1/2 -translate-y-1/2" id = "VisualBar">
           <AddExpense triggerRerender={triggerRerender}/>
         </div>
 
-        <div className = "block fixed top-[76%] left-2/7 w-[30%] h-[40%] min-h-fit bg-[rgba(17,18,23,0.9)] border border-[#6d91e8] border-[1.5px] rounded-[2%] text-center transform -translate-x-1/2 -translate-y-1/2" id = "IncomeListBar">
+        <div className = "block fixed top-[78%] left-2/7 w-[30%] h-[40%] min-h-fit bg-[rgba(17,18,23,0.9)] border border-[#6d91e8] border-[1.5px] rounded-[2%] text-center transform -translate-x-1/2 -translate-y-1/2" id = "IncomeListBar">
           <IncomeList triggerRerender={triggerRerender}/>
         </div>
 
-        <div className = "block fixed top-[76%] left-[70%] w-[30%] h-[40%] min-h-fit bg-[rgba(17,18,23,0.9)] border border-[#6d91e8] border-[1.5px] rounded-[2%] text-center transform -translate-x-1/2 -translate-y-1/2" id = "ExpensesListBar">
+        <div className = "block fixed top-[78%] left-[70%] w-[30%] h-[40%] min-h-fit bg-[rgba(17,18,23,0.9)] border border-[#6d91e8] border-[1.5px] rounded-[2%] text-center transform -translate-x-1/2 -translate-y-1/2" id = "ExpensesListBar">
           <ExpensesList triggerRerender={triggerRerender}/>
         </div>
     </div>
